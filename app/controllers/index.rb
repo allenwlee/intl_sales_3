6dd7e45c2f97cfaa0d31d4fa4828c60f59aa2988
Allen Lee <allenwlee@Allens-MacBook-Pro.local>
2013-05-08 00:22:14 -0700

    login

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,5 +4,29 @@ get '/' do
 end
 
 post '/sales' do
-  Sale.create(territory: params[:territory], bid: params[:bid], ask: params[:ask], close: params[:close], close_date: params[:close_date])
+  @sale = Sale.create(territory: params[:territory], bid: params[:bid], ask: params[:ask], close: params[:close], close_date: params[:close_date])
+  @user = User.find_by_id[session[:id]]
+  @user.sales << @sale
+  erb :index
+end
+
+get '/create' do
+  erb :create
+end
+
+post '/create' do
+  @user = User.new(email: params[:email], password: params[:password])
+  if @user.validate?
+    @user.save!
+    session[:user_id] = @user.id 
+    redirect '/'
+  else
+    @error = "Invalid Username, Email or Password"
+    redirect '/create'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
   has_many :sales
+
+  include BCrypt
+
+  def self.authenticate(username, password)
+    user = User.find_by_username(username)
+    return nil unless user
+    db_password = Password.new(user.password)
+    db_password == password ? user : nil
+  end
+
+  private
+
+  def hash_password
+    self.password = Password.create(self.password)
+  end
 end
